9f13cff5ea7836b381dc5d0c72e2d93e057a3a39
Rick <web8036@caelum131-15>
2019-01-29 21:03:59 -0200

    servlet final

--- fj21-agenda/src/br/com/caelum/mvc/logica/RemoveContatoLogic.java
+++ fj21-agenda/src/br/com/caelum/mvc/logica/RemoveContatoLogic.java
@@ -1,5 +1,7 @@
 package br.com.caelum.mvc.logica;
 
+import java.sql.Connection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,20 +10,22 @@ import br.com.caelum.agenda.modelo.Contato;
 
 public class RemoveContatoLogic implements Logica {
 	
-	public String executa(HttpServletRequest req, HttpServletResponse res)
+	public String executa(HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
 		
-			long id = Long.parseLong(req.getParameter("id"));
+			long id = Long.parseLong(request.getParameter("id"));
 			
 			Contato contato = new Contato();
 			contato.setId(id);
 			
-			ContatoDao dao = new ContatoDao();
+			Connection connection = (Connection) request.getAttribute("conexao");
+			
+			ContatoDao dao = new ContatoDao(connection);
 			dao.exclui(contato);
 			
 			System.out.println("Excluindo contato ... ");
 			
-			return "lista-contatos.jsp";
+			return "mvc?logica=ListaContatosLogic";
 	}
 
 }
