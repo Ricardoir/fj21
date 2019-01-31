bdb34d4865ce1e0b82c4e38eff201d6177b13a44
Rick <web8036@caelum131-15>
2019-01-31 21:03:23 -0200

    Spring_III

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -36,5 +36,25 @@ public class TarefasController {
 		return "tarefa/lista";
 	}
 	
+	@RequestMapping("removeTarefa")
+	public String remove(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.remove(tarefa);
+		return "redirect:listaTarefas";
+	}
+	
+	@RequestMapping("mostraTarefa")
+	public String mostra(Long id, Model model) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefa", dao.buscaPorId(id));
+		return "tarefa/mostra";
+	}
+	
+	@RequestMapping("alteraTarefa")
+	public String altera(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.altera(tarefa);;
+		return "redirect:listaTarefas";
+	}
 	
 }
